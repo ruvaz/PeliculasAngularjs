0d774b416a07612738fd2c7c6f889e8a84f804e5
Ruben Vazquez <ruben@medtrainer.com>
2017-10-24 13:30:04 -0500

    save changes

--- app/app.component.js
+++ app/app.component.js
@@ -1,4 +1,4 @@
-System.register(['angular2/core', "./components/peliculas-list.component", "./components/peliculas-footer.component"], function(exports_1, context_1) {
+System.register(["angular2/router"], function(exports_1, context_1) {
     "use strict";
     var __moduleName = context_1 && context_1.id;
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
@@ -10,31 +10,22 @@ System.register(['angular2/core', "./components/peliculas-list.component", "./co
     var __metadata = (this && this.__metadata) || function (k, v) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
-    var core_1, peliculas_list_component_1, peliculas_footer_component_1;
+    var router_1;
     var AppComponent;
     return {
         setters:[
-            function (core_1_1) {
-                core_1 = core_1_1;
-            },
-            function (peliculas_list_component_1_1) {
-                peliculas_list_component_1 = peliculas_list_component_1_1;
-            },
-            function (peliculas_footer_component_1_1) {
-                peliculas_footer_component_1 = peliculas_footer_component_1_1;
+            function (router_1_1) {
+                router_1 = router_1_1;
             }],
         execute: function() {
             // Decorador component, indicamos en que etiqueta se va a cargar la plantilla
+            ;
             AppComponent = (function () {
                 function AppComponent() {
                     this.titulo = "Peliculas con angular2";
                 }
                 AppComponent = __decorate([
-                    core_1.Component({
-                        selector: 'my-app',
-                        templateUrl: "app/view/peliculas.html",
-                        directives: [peliculas_list_component_1.PeliculasListComponent, peliculas_footer_component_1.FooterComponent] //array de todos los componentes que se desean utilizar
-                    }), 
+                    router_1.RouteConfig, 
                     __metadata('design:paramtypes', [])
                 ], AppComponent);
                 return AppComponent;
--- app/app.component.ts
+++ app/app.component.ts
@@ -1,17 +1,37 @@
 // Importar el núcleo de Angular
 import {Component} from 'angular2/core';
 // import {Pelicula} from './model/pelicula';
-import {PeliculasListComponent} from   "./components/peliculas-list.component";
+import {PeliculasListComponent} from "./components/peliculas-list.component";
 import {FooterComponent} from "./components/peliculas-footer.component";
+
+//configurar rutas
+import {ROUTER_DIRECTIVES, ROUTER_PROVIDERS} from "angular2/router";
+
+import {RouteConfig} from "angular2/router";
+
+import {Router} from "angular2/router";
+import {Pelicula} from "./model/pelicula";
 
 // Decorador component, indicamos en que etiqueta se va a cargar la plantilla
 @Component(
     {
         selector: 'my-app',
         templateUrl: `app/view/peliculas.html`,
-        directives: [PeliculasListComponent,FooterComponent] //array de todos los componentes que se desean utilizar
+        directives: [PeliculasListComponent, FooterComponent, ROUTER_DIRECTIVES] //array de todos los componentes que se desean utilizar
     }
 )
+
+@RouterConfig(
+    {
+{ 'path':'/peliculas',
+        'name':'Peliculas',
+    'component':PeliculasListComponent,
+    'useAsDefault':true },
+},
+);
+
+
+@RouteConfig
 
 // Clase del componente donde iran los datos y funcionalidades
 export class AppComponent {
